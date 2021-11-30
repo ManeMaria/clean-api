267ad30740f90565ef4b954974064c9f90c33dfe
César Damasceno <mirandacesar598@gmail.com>
2021-11-29 21:36:26 -0300

    test: se não vinher os parâmetros(email) retorna um erro

--- src/apresentation/controllers/signup.spec.ts
+++ src/apresentation/controllers/signup.spec.ts
@@ -20,3 +20,24 @@ describe('SignUp Controller', () => {
     expect(htttpResponse.body).toEqual(new Error('Parâmetros não fornecidos'))
   })
 })
+
+describe('SignUp Controller', () => {
+  test('se não enviar um email, será retornado erro 400', () => {
+    // coloca a classe de sut (sistem unde test) como prefixo para
+    // indicar qual classe estamos testando
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        // email: 'any@email@email.com',
+        password: 'any-pss',
+        passwordConfirmation: 'any-pss'
+      }
+    }
+    const htttpResponse = sut.handle(httpRequest)
+    // tobe compara os objetos em si
+    expect(htttpResponse.statusCode).toBe(400)
+    // tobe compara os valores dos objetos em si
+    expect(htttpResponse.body).toEqual(new Error('Parâmetros não fornecidos'))
+  })
+})
