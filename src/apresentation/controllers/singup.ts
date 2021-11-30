98464a771fc9b7268dc2438ea698c96822b3bb34
César Damasceno <mirandacesar598@gmail.com>
2021-11-29 21:07:32 -0300

    Revert "Revert "chore: add husky and lint-staged""
    This reverts commit 747f21420fe839e4428c84d687d34af69e06afdb.

---
+++ src/apresentation/controllers/singup.ts
@@ -0,0 +1,10 @@
+
+// lembrar de sempre commitar primeiro o arquivo de produção antes
+// do arquivo de teste
+export class SignUpController {
+  handle (httpRequest: any): any {
+    return {
+      statusCode: 400
+    }
+  }
+}
