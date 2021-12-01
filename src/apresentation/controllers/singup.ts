ab90999cf3efe59419ba065d2cf616e00ee453b8
César Damasceno <mirandacesar598@gmail.com>
2021-12-01 05:14:06 -0300

    refactor: garantindo a implementação correta da interface do SignUpController

--- src/apresentation/controllers/singup.ts
+++ src/apresentation/controllers/singup.ts
@@ -1,8 +1,8 @@
-
+import * as i from '../protocols/http'
 // lembrar de sempre commitar primeiro o arquivo de produção antes
 // do arquivo de teste
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: i.HttpRequest): i.HttpResponse {
     const { body: { name, email } } = httpRequest
     if (!name || !email) {
       return {
---
+++ src/apresentation/protocols/http.ts
@@ -0,0 +1,10 @@
+// interfaces para a classe SignUp
+
+export interface HttpResponse {
+  statusCode: number
+  body: any
+}
+
+export interface HttpRequest {
+  body?: any
+}
