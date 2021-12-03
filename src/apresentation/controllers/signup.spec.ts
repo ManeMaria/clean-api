8a96a280fabeb8b0ca9e57e6faddefce98dcf7b4
César Damasceno <mirandacesar598@gmail.com>
2021-12-02 21:14:08 -0300

    test: refatoração do teste

--- src/apresentation/controllers/signup.spec.ts
+++ src/apresentation/controllers/signup.spec.ts
@@ -20,11 +20,7 @@ describe('SignUp Controller', () => {
     // tobe compara os valores dos objetos em si
     expect(htttpResponse.body).toEqual(new MissingParamsError('email'))
   })
-})
 
-// lembrar de sempre commitar primeiro o arquivo de produção antes
-// do arquivo de teste
-describe('SignUp Controller', () => {
   test('se não enviar um nome, será retornado erro 400', () => {
     // coloca a classe de sut (sistem unde test) como prefixo para
     // indicar qual classe estamos testando
@@ -42,11 +38,25 @@ describe('SignUp Controller', () => {
     // tobe compara os valores dos objetos em si
     expect(htttpResponse.body).toEqual(new MissingParamsError('name'))
   })
-})
 
-// lembrar de sempre commitar primeiro o arquivo de produção antes
-// do arquivo de teste
-describe('SignUp Controller', () => {
+  test('se não enviar um passwordConfirmation, será retornado erro 400', () => {
+    // coloca a classe de sut (sistem unde test) como prefixo para
+    // indicar qual classe estamos testando
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any@email@email.com',
+        password: 'any-pss'
+      }
+    }
+    const htttpResponse = sut.handle(httpRequest)
+    // tobe compara os objetos em si
+    expect(htttpResponse.statusCode).toBe(400)
+    // tobe compara os valores dos objetos em si
+    expect(htttpResponse.body).toEqual(new MissingParamsError('passwordConfirmation'))
+  })
+
   test('se não enviar um password, será retornado erro 400', () => {
     // coloca a classe de sut (sistem unde test) como prefixo para
     // indicar qual classe estamos testando
@@ -64,11 +74,7 @@ describe('SignUp Controller', () => {
     // tobe compara os valores dos objetos em si
     expect(htttpResponse.body).toEqual(new MissingParamsError('password'))
   })
-})
 
-// lembrar de sempre commitar primeiro o arquivo de produção antes
-// do arquivo de teste
-describe('SignUp Controller', () => {
   test('se não enviar um passwordConfirmation, será retornado erro 400', () => {
     // coloca a classe de sut (sistem unde test) como prefixo para
     // indicar qual classe estamos testando
