db67d275281b4004ce452c9e3296d9724ba12a34
César Damasceno <mirandacesar598@gmail.com>
2021-12-03 22:04:46 -0300

    test: o SignUpController retorna um erro 400 se o email for inválido

--- src/apresentation/controllers/signup.spec.ts
+++ src/apresentation/controllers/signup.spec.ts
@@ -1,12 +1,34 @@
 import { SignUpController } from './singup'
 import { MissingParamsError } from '../erros/missing-parrams-erros'
+import { InvalidParamError } from '../erros/invalid-parrams-error'
+import { EmailValidator } from '../protocols/email-validator'
 
-const makeSut = (): SignUpController => new SignUpController()
+interface SutTypes {
+  sut: SignUpController
+  emailValidator: EmailValidator
+}
+const makeSut = (): SutTypes => {
+  // test buble, um tipo de moke, um função que retorna um valor certo
+  // se criou um moke, pois a intenção do teste é apenas realizar uma função baseado
+  // na resposta do validador
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  const emailValidator = new EmailValidatorStub()
+  return {
+    sut: new SignUpController(emailValidator),
+    emailValidator
+  }
+}
+
 describe('SignUp Controller', () => {
   test('se não enviar um email, será retornado erro 400', () => {
     // coloca a classe de sut (sistem unde test) como prefixo para
     // indicar qual classe estamos testando
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -24,7 +46,7 @@ describe('SignUp Controller', () => {
   test('se não enviar um nome, será retornado erro 400', () => {
     // coloca a classe de sut (sistem unde test) como prefixo para
     // indicar qual classe estamos testando
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any@email@email.com',
@@ -42,7 +64,7 @@ describe('SignUp Controller', () => {
   test('se não enviar um passwordConfirmation, será retornado erro 400', () => {
     // coloca a classe de sut (sistem unde test) como prefixo para
     // indicar qual classe estamos testando
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -60,7 +82,7 @@ describe('SignUp Controller', () => {
   test('se não enviar um password, será retornado erro 400', () => {
     // coloca a classe de sut (sistem unde test) como prefixo para
     // indicar qual classe estamos testando
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -75,21 +97,24 @@ describe('SignUp Controller', () => {
     expect(htttpResponse.body).toEqual(new MissingParamsError('password'))
   })
 
-  test('se não enviar um passwordConfirmation, será retornado erro 400', () => {
+  test('validadndo email', () => {
     // coloca a classe de sut (sistem unde test) como prefixo para
     // indicar qual classe estamos testando
-    const sut = makeSut()
+    const { sut, emailValidator } = makeSut()
+
+    jest.spyOn(emailValidator, 'isValid').mockReturnValueOnce(false)
     const httpRequest = {
       body: {
         name: 'any_name',
-        email: 'any@email@email.com',
-        password: 'any-pss'
+        email: 'invali_email@email@email.com',
+        password: 'any-pss',
+        passwordConfirmation: 'any-pss'
       }
     }
     const htttpResponse = sut.handle(httpRequest)
     // tobe compara os objetos em si
     expect(htttpResponse.statusCode).toBe(400)
     // tobe compara os valores dos objetos em si
-    expect(htttpResponse.body).toEqual(new MissingParamsError('passwordConfirmation'))
+    expect(htttpResponse.body).toEqual(new InvalidParamError('email'))
   })
 })
