e2f163d30d7d66035872f8a6c6eab47563028a31
César Damasceno <mirandacesar598@gmail.com>
2021-12-14 21:35:14 -0300

    test: criação isValid,

--- src/apresentation/utils/email-validator/email.validator.spec.ts
+++ src/apresentation/utils/email-validator/email.validator.spec.ts
@@ -1,8 +1,22 @@
+
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+jest.mock('validator', () => (
+  {
+    isEmail: (): boolean => true
+  }
+))
 
 describe('EmailValidator adapter', () => {
+  it('should return true if is not validated', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@email.com')
+    expect(isValid).toBe(true)
+  })
+
   it('should return false if is not validated', () => {
     const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@email.com')
     expect(isValid).toBe(false)
   })
