455a8b626ef896da941548d683c5f0edb25163e7
César Damasceno <mirandacesar598@gmail.com>
2021-12-19 20:22:47 -0300

    test:  verificação se o valor de retorno é igual

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  hash: async (): Promise<string> => {
+    return new Promise((resolve, reject) => resolve('hash'))
+  }
+}))
+
 describe('Bcrypt adapter', () => {
   it('deve receber os valores corretos ', async () => {
     const salt = 12
@@ -8,5 +14,12 @@ describe('Bcrypt adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypter('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('deve retornar a hash em caso de sucesso', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypter('any_value')
+    expect(hash).toBe('hash')
   })
 })
