415b6c09df44a6ae598c40f511ac563a481c3ee2
César Damasceno <mirandacesar598@gmail.com>
2021-12-19 20:34:52 -0300

    refactor: movendo a criação de instâncias

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,16 +1,21 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+const salt = 12
+
 jest.mock('bcrypt', () => ({
   hash: async (): Promise<string> => {
     return new Promise((resolve, reject) => resolve('hash'))
   }
 }))
 
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
 describe('Bcrypt adapter', () => {
   it('deve receber os valores corretos ', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypter('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
@@ -19,8 +24,7 @@ describe('Bcrypt adapter', () => {
   // o método hash foi simulado com uma valor qualquer, pois o que interessa é se ele retorna um valor,
   // em caso de sucesso, não como ele encripta um valor
   it('deve retornar a hash em caso de sucesso', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hash = await sut.encrypter('any_value')
     expect(hash).toBe('hash')
   })
