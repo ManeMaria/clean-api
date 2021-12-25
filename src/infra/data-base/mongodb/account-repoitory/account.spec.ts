e48e7c27e5b10a8f1944bf462ccf85987af530c0
César Damasceno <mirandacesar598@gmail.com>
2021-12-25 19:54:44 -0300

    test: retorno do account c/ id em caso de sucesso

---
+++ src/infra/data-base/mongodb/account-repoitory/account.spec.ts
@@ -0,0 +1,43 @@
+
+// import * as p from '../../../../domain/usecase/add-account'
+import { AccountMongoRepository } from './account'
+// import { AccountModel } from '../../../../domain/models/account'
+import { mongoHelper } from '../helpers/mongo-helper'
+
+const makeSut = (): AccountMongoRepository => {
+  // class AccountMongoRepository implements p.AddAccount {
+  //   async add (account: p.AddAccountModel): Promise<AccountModel> {
+  //     return new Promise((resolve, reject) => resolve({
+  //       id: 'any_id',
+  //       name: 'any_name',
+  //       email: 'any_email@any.com',
+  //       password: 'any_password'
+  //     }))
+  //   }
+  // }
+
+  return new AccountMongoRepository()
+}
+describe('Conta no Mongo repository', () => {
+  beforeAll(async () => {
+    await mongoHelper.connect()
+  })
+  afterAll(async () => {
+    await mongoHelper.disconnect()
+  })
+  test('Deve retornar uma conta em caso de sucesso', async () => {
+    const sut = makeSut()
+
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@any.com',
+      password: 'any_password'
+    })
+    // verifica um por um, posi não me interessa saber como será o hash do id
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@any.com')
+    expect(account.password).toBe('any_password')
+  })
+})
