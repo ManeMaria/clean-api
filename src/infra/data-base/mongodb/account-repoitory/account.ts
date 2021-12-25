ff1e53eb248cad752dc6d8123b1522adc3d61204
César Damasceno <mirandacesar598@gmail.com>
2021-12-25 19:54:28 -0300

    feat: retorno do account c/ id em caso de sucesso

---
+++ src/infra/data-base/mongodb/account-repoitory/account.ts
@@ -0,0 +1,16 @@
+
+import { AccountModelData } from '../../../../data/protocols/account-data'
+import { AddAccountModelData } from '../../../../data/protocols/add-account-data'
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { mongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModelData): Promise<AccountModelData> {
+    const accountColletion = mongoHelper.getColletion('accounts')
+    await accountColletion.insertOne(accountData)
+    // @ts-expect-error
+    const { _id, ...accountWithOutId } = accountData
+    const refactorResult = { ...accountWithOutId, id: _id.toString() }
+    return refactorResult
+  }
+}
--- src/infra/data-base/mongodb/helpers/mongo-helper.ts
+++ src/infra/data-base/mongodb/helpers/mongo-helper.ts
@@ -1,25 +1,27 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
+
+let connection, dB
 
 export const mongoHelper = {
   // vê se faz diferença
 //   client: null as MongoClient,
-  client: MongoClient,
-  db: null,
-  connect: async () => {
-    // @ts-expect-error
-    this.client = await MongoClient.connect(global.__MONGO_URI__, {
+  // client: null as MongoClient,
+  // db: null,
+  connect: async function () {
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
       // @ts-expect-error
-      // o types do mongo não está sendo lido
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
-    // @ts-expect-error
-    this.db = await this.client.db(global.__MONGO_DB_NAME__)
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    dB = await connection.db(global.__MONGO_DB_NAME__)
   },
-  disconnect: async () => {
-    // @ts-expect-error
-    await this.client.close()
-    // @ts-expect-error
-    await this.db.close()
+  disconnect: async function () {
+    await connection.close()
+  },
+
+  getColletion: function (name: string): Collection {
+    return connection.db().collection(name)
   }
 }
