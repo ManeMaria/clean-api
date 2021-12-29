dc50e7b543e85a63b1b78ab375f088c79c3d82ed
César Damasceno <mirandacesar598@gmail.com>
2021-12-29 05:43:55 -0300

    refactor: centralização nos caminhos dos middlewares

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express'
-import { bodyParse } from '../middlesware/body-parse/body-parse'
-import { contentType } from '../middlesware/content-type/content-type'
-import { cors } from '../middlesware/cors/cors'
+import { bodyParse, contentType, cors } from '../middlesware'
 // arquivo é para usar os middlewares.
 
 export default (app: Express): void => {
---
+++ src/main/middlesware/index.ts
@@ -0,0 +1,4 @@
+
+export * from './content-type/content-type'
+export * from './cors/cors'
+export * from './body-parse/body-parse'
